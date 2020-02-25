adef0c3aa401ff5a80839c75b1f3bb6050c0aa5e
ansh8tu <anshu8tu@gmail.com>
2020-02-25 22:47:00 +0530

    Add project files.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,47 @@
+/*
+//This is the program for using const operator
+#include<iostream>
+using namespace std;
+
+const double price_per_room = 30.0;
+const double tax_per_room = 0.06;
+
+
+int main()
+{
+	int number_of_rooms;
+	cout<< "Enter the number of rooms: ";
+	cin >> number_of_rooms;
+
+	cout << "The price per room is:$ " << price_per_room<<endl;
+	double cost = number_of_rooms * price_per_room;
+	cout << "The total price of room is:$ " << cost << endl;
+
+	double total_tax = cost * tax_per_room;
+	cout << "The tax is:$ " << total_tax<< endl;
+
+	double total_cost = cost + total_tax;
+	cout << "The total expense is:$ " << total_cost << endl;
+
+	return 0;
+}
+*/ 
+/*#include<iostream>
+using namespace std;
+
+int main()
+{
+	//This is the program for declaring and initializing an array
+	int test_scores[] = { 10,20,30,40,50 };
+	cout << "Enter the marks that Roll Number 0 expected: ";
+	cin >> test_scores[0];
+
+	//Setting the value of marks scored by Roll number 0 to 35
+	test_scores[0] = 35;
+	cout <<"The marks scored by Roll Number 0 is: " <<test_scores[0] << endl;
+	
+}*/
+
+
+
+
