70e19401aad64fa700d63247a9acc76fb11b410e
ansh8tu <anshu8tu@gmail.com>
2020-02-25 23:08:05 +0530

    This is just a random commit to check whether a certain commit is working or not

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -42,6 +42,17 @@ int main()
 	
 }*/
 
+#include<iostream>
+#include<vector>
+using namespace std;
 
+int main()
+{
+	vector <char> vowels{ 'a','e','i','o','u' };
+	vector <int> test_scores{ 10,20,30,40 };
+	vector <double> days_in_year(365, 90);
+
+
+}
 
 
