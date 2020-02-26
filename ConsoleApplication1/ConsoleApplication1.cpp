02d34e3cba646939eb42061f5657c2c15d3d9582
ansh8tu <anshu8tu@gmail.com>
2020-02-26 16:34:21 +0530

    The use of static_cast<double>(variable name)

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -115,8 +115,7 @@ int main()
 
 }
 */
-
-#include<iostream>
+/*#include<iostream>
 using namespace std;
 
 int main()
@@ -132,6 +131,28 @@ int main()
 
 	cout << "The value of result is: " << result;
 	return 0;
+}*/
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	const int count = 3;
+	int num1, num2, num3;
+	int sum;
+
+	cout << "Enter three integers seperated by spaces";
+	cin >> num1 >> num2 >> num3;
+
+	sum = num1 + num2 + num3;
+	cout << "The sum of numbers is: " << sum<<endl;
+
+	double average;
+	average = static_cast<double>(sum)/count;
+
+	cout << "The average of numbers is: " << average << endl;
+
 }
 
-
+ 
