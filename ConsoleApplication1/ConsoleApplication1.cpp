f366037cec7b13e9d6738cd9ce803c05a5c27096
ansh8tu <anshu8tu@gmail.com>
2020-02-26 22:11:58 +0530

    Added code for logical operators

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -132,7 +132,7 @@ int main()
 	cout << "The value of result is: " << result;
 	return 0;
 }*/
-
+/*
 #include<iostream>
 using namespace std;
 
@@ -153,6 +153,27 @@ int main()
 
 	cout << "The average of numbers is: " << average << endl;
 
-}
-
+} 
+*/
  
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	const int lower = 10;
+	const int upper = 20;
+	int number;
+	cout << boolalpha;
+
+	cout << "Enter a number between: " << lower << "and " << upper;
+	cin >> number;
+
+	bool in_bounds=false; 
+
+	in_bounds = (number < lower || number >= upper);
+
+	cout << "The number is outside " << lower << "and " << upper<<":" << in_bounds << endl;
+
+
+}
