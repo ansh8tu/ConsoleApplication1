896b12a3c23ac22e59d32b4e83c6e087024b77d9
ansh8tu <anshu8tu@gmail.com>
2020-03-01 01:22:53 +0530

    Added Code for range based for loop

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -266,11 +266,34 @@ int main()
 	vector<int> nums{ 10,20,30,40,50 };
 
 	for (unsigned int i = 0; i < nums.size(); i++)
-		cout << nums.at(i) << endl;	
+		cout << nums.at(i) << endl;
 }
-
  
+===========================================================================================================================================
+//This is a code for range based for loop
+===========================================================================================================================================
 
+#include<iostream>
+#include<vector>
+#include<iomanip>
+using namespace std;
+
+int main()
+{
+	vector<double>temperature{ 20.33,45.66,65.66 };
+	double average{};
+	double sum{};
+	for (auto temp : temperature)
+	{
+		sum += temp;
+		cout << "The sum of temperatures is: " << sum << endl;
+	}
+
+	average = sum/ temperature.size();
+	cout << fixed << setprecision(1);
+	cout << "The average is: " << average << endl;
+	
+}
 
 
 
