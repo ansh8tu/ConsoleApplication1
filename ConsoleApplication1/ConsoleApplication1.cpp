8be15356ce0702fc3af2568bceab11abfcd3cb99
ansh8tu <anshu8tu@gmail.com>
2020-03-01 17:24:02 +0530

    Added program for .find(string name)

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -383,3 +383,30 @@ int main()
 	cout << "Hello" << last_name << "your name has " << strlen(last_name) << "characters" << endl;
 
 }
+
+=================================================================================================================================================
+//This is a program for .find () function in string
+=================================================================================================================================================
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+	string s1 = "The word to find is boo";
+
+	string word;
+	cout << "Enter the word you want to find: ";
+	cin >> word;
+
+	int position = s1.find(word);
+
+	if (position != string::npos)
+		cout << "Word found at position: " << position << endl;
+	else
+	{
+		cout << "Sorry word not foound" << endl;
+
+	}
+
+}
